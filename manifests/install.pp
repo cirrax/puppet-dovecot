ea0611b8b95ef30bb987c0dad53b73616ff69128
Evan Booyens <43237322+sl0m0ZA@users.noreply.github.com>
2022-10-10 10:11:44 +0200

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,13 @@
 #
 # @param packages
 #   Array of packages to install
+#   Example (hiera):  
+#     dovecot::install:  
+#       - dovecot-core  
+#       - dovecot-imapd  
+#       - dovecot-pop3d  
+#  
+#  Will install current versions of dovecot-core,dovecot-imapd and dovecot-pop3d
 # @param package_ensure
 #   what to ensure for package
 #   Defaults to 'installed'
